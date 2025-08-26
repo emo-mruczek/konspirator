bdab3caf09ce961fe0368939026cab93704d831e
emo-mruczek <krokcia1@gmail.com>
2025-08-26 19:02:20 +0200

    add: reading command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 mod ast;
+
 use lalrpop_util::lalrpop_mod;
+use std::{env, process::exit};
+
 lalrpop_mod!(parser);
 
 fn main() {
-    
+    let args: Vec<String> = env::args().collect();
 
-    println!("Hello, world!");
+    if args.len() != 3 {
+        eprintln!("\x1b[31mProvide 2 arguments!\x1b[0m");
+        exit(1);
+    }
+
+    let in_name = &args[1];
+    let out_name = &args[2];
+
+
+    println!(" Input file: {}\n Output file {}", in_name, out_name);
 }
